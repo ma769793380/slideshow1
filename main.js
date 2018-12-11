bca45f464d2dc6adf37edb6587960257432fec62
maliang <1210223813@qq.com>
2018-12-11 23:53:52 +0800

    轮播一

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2018/12/11.
+ */
+$('.images > img:nth-child(1)').addClass('current');
+$('.images > img:nth-child(2)').addClass('enter');
+$('.images > img:nth-child(3)').addClass('enter');
+
+let n=1;
+function x(n){
+    if(n>3){
+        n=n%3;
+        if(n===0){
+            n = 3
+        }
+    }
+    return n
+}
+setInterval(function(){
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+        .one('transitionend',function(e){
+            $(e.currentTarget).removeClass('leave').addClass('enter')
+        });
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+    n+=1
+},2000);
